231ade3b806c7fded9ce10fbc258ab2b19e2f5cd
florin.bentz <ovidiubentz#gmail.com>
2024-04-11 19:22:16 +0300

    concatenation exemple

--- C06-String/C06-String/Program.cs
+++ C06-String/C06-String/Program.cs
@@ -10,5 +10,15 @@
         Console.WriteLine(myNumber);
 
         string myNumberAsString = myNumber.ToString();
+
+        //Manipulating strings;
+        //Concatenation
+
+        string firstName = "Bentz";
+        string lastName = "Ovidiu Florin";
+        string fullName = firstName + " " + lastName; 
+        string fullNameWithConcat = string.Concat(firstName," ", lastName);
+        Console.WriteLine(fullNameWithConcat);
+        Console.WriteLine($"{firstName} {lastName}"); //string interpolation
     }
 }
