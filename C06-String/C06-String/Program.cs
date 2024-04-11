d51e7496509495cd36c51e32285d3e11d08db78b
florin.bentz <ovidiubentz#gmail.com>
2024-04-11 19:00:40 +0300

    initial comit string courses

---
+++ C06-String/C06-String/Program.cs
@@ -0,0 +1,14 @@
+﻿public static class Program
+{
+    public static void Main(string[] args)
+    {
+        //Decalre strings
+        string myFirstString = "Hello world";
+        Console.WriteLine(myFirstString);
+
+        int myNumber = 42;
+        Console.WriteLine(myNumber);
+
+        string myNumberAsString = myNumber.ToString();
+    }
+}
